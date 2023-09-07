9085bb7b811bbaee7632bd78c4b9f5b697412693
robpon <86128798+robpon@users.noreply.github.com>
2023-09-07 19:19:29 +0200

    Create zadanie-probne.cpp

---
+++ zadanie-probne.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int x;
+    cin >> x;
+    int y;
+    cin >> y;
+    cout << x + y;
+    return 0;
+}
+
